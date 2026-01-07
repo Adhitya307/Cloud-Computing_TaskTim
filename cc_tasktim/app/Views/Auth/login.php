d97081e86c7f0aab3481431f993b09295d918a75
Adhitya307 <adhityabagus509@gmail.com>
2026-01-08 03:23:40 +0700

    6

--- cc_tasktim/app/Views/Auth/login.php
+++ cc_tasktim/app/Views/Auth/login.php
@@ -108,7 +108,7 @@
 <body>
 
   <form method="post" action="/login">
-    <h2>Login duluu bgggg</h2>
+    <h2>Login </h2>
 
     <input type="email" name="email" placeholder="Email" required>
     <input type="password" name="password" placeholder="Sandi" required>
